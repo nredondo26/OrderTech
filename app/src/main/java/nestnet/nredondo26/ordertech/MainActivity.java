d1a5230ff81ad283516722e256c0cb6c16fa8de6
Nelson Enrique Redondo Barros <30601611+nredondo26@users.noreply.github.com>
2018-04-24 17:52:53 -0500

    Initial commit

---
+++ app/src/main/java/nestnet/nredondo26/ordertech/MainActivity.java
@@ -0,0 +1,13 @@
+package nestnet.nredondo26.ordertech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
